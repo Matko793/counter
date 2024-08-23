879212d269e2c10107b77bca4c3326d29175a2e1
matko793 <matejhric2005@gmail.com>
2024-08-23 17:39:36 +0200

    Pridal som javascrp ktory funguje

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("ahoj")
+
+    const abraka = document.querySelector("h2");
+    let num = 0;
+
+    document.getElementById("inc").onclick = function test(){
+        num++
+        abraka.textContent = num;  
+    }
+    document.getElementById("rst").onclick = function reset(){
+        num = 0;
+        abraka.textContent = num;
+    }
+    
+
+  
