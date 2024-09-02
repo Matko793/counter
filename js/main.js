55399e6c6e1cd8e7a53734dd50b62ef9f2711804
matko793 <matejhric2005@gmail.com>
2024-09-02 10:01:48 +0200

    Pridal som kraaaaaaaaaaasny prechod medzi light/dark modom

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 
     //variables
+    var slideSource = document.getElementById('slideSource');
+
+
 
     let abraka = document.querySelector("h2");
     let num = 0;
+
     //functions
     function plus(){
         num++
@@ -17,13 +21,25 @@
             abraka.textContent = num;
         }  
     }
-    function reset(){
+    function    reset(){
         num = 0;
         abraka.textContent = num;
     }
     function mode(){
         let element = document.getElementById("myBody");
+        element.classList.add("fade");
         element.classList.toggle("darkmode");
+        // Počkajte chvíľu, aby prechod začal a potom zmeníme režim
+        setTimeout(() => {
+            element.classList.add("fade-in");      // Pridaj triedu pre viditeľnosť po zmene módu
+            }, 100);  // Nastavenie veľmi krátkeho oneskorenia
+
+    // Po dokončení prechodu odstráň .fade triedu
+        setTimeout(() => {
+            element.classList.remove("fade");
+            element.classList.remove("fade-in");
+            }, 120);  // Odstráň triedy po 2 sekundách (dĺžka prechodu)
+
     }
 
     // assining functions
